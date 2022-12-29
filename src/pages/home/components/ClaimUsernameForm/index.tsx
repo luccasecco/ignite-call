eb65a96e67e711ef9b0a62f20ad6cd4b34aaa495
luccasecco <luccasecco@hotmail.com>
2022-12-29 20:25:22 -0300

    feat: estrutura visual da home

---
+++ src/pages/home/components/ClaimUsernameForm/index.tsx
@@ -0,0 +1,32 @@
+import { Button, TextInput } from '@ignite-ui/react'
+import { ArrowRight } from 'phosphor-react'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+import { Form } from './styles'
+
+const claimUsernameFormSchema = z.object({
+  username: z.string(),
+})
+
+type ClaimUsernameFormData = z.infer<typeof claimUsernameFormSchema>
+
+export function ClaimUsernameForm() {
+  const { register, handleSubmit } = useForm<ClaimUsernameFormData>()
+
+  async function handleClaimUsername(data: ClaimUsernameFormData) {}
+
+  return (
+    <Form as="form" onSubmit={handleSubmit(handleClaimUsername)}>
+      <TextInput
+        size="sm"
+        prefix="ignite.com/"
+        placeholder="seu-usuário"
+        {...register('username')}
+      />
+      <Button size="sm" type="submit">
+        Reservar
+        <ArrowRight />
+      </Button>
+    </Form>
+  )
+}
